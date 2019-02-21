c015904f2d9de82e93859f3cf65c8b424746a7f3
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2019-02-20 23:06:32 -0500

    add fake sites list & some aux functions

--- warc_aux.py
+++ warc_aux.py
@@ -1,4 +1,7 @@
 from warcio.archiveiterator import ArchiveIterator
+import boto3
+import botocore
+import csv
 
 def getSetOfSites(filename):
     result = set()
@@ -21,3 +24,32 @@ def getSetOfSites(filename):
                         break
     return result
 
+def downloadSegment(remote_path, save_path):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket("commoncrawl")
+    bucket.download_file(remote_path, save_path)
+
+def loadSiteList(csv_path):
+    result = list()
+    with open(csv_path) as f:
+        csvreader = csv.reader(f)
+        for row in csvreader:
+            result.append(row[0].lower())
+    return result
+
+def matchSite(cite_list, a_url):
+    # return the matching site in the list (if found)
+    # return None otherwise
+    a_url = a_url.lower()
+    for c in cite_list:
+        if c in a_url:
+            return c
+    return None
+
+def storeInSQL():
+    # TBD
+    pass
+
+def handleOneSegment(warc_file_path):
+    # TBD
+    pass
